870caff36cf3a49a02ec52b99276cd7260b131a1
James Gill <jamesgill347@gmail.com>
2020-11-30 16:57:48 -0500

    Navigation and Camera set up, nav working but camera still needs to be fixed.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler';
 
 import App from './App';
+import Routes from './Routes'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Routes);
