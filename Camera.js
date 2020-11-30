ff5bd2b1f0722a4300eb050c9144e21aefcfbd0c
James Gill <jamesgill347@gmail.com>
2020-11-30 18:54:12 -0500

    working camera.  fix is success.

--- Camera.js
+++ Camera.js
@@ -1,3 +1,4 @@
+import { NavigationContext } from '@react-navigation/native';
 import React from 'react'
 import { StyleSheet, View, Image, Platform, Text, TouchableOpacity, AppRegistry } from 'react-native';
 import { RNCamera } from 'react-native-camera';
@@ -39,13 +40,17 @@ class Camera extends React.Component {
       }
     
       takePicture = async () => {
-        if (this.camera) {
-          const options = { quality: 0.5, base64: true };
-          const data = await this.camera.takePictureAsync(options);
-          console.log(data.uri);
-        }
-      };
+        try {
+            if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options);
+            console.log(data.uri);
+            }
+        } catch (error) {
+            console.error(error)
     }
+      };
+}
     
     const styles = StyleSheet.create({
       container: {
