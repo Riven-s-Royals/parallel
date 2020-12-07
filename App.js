1925c300c751289f9301fb2b5bab0e6966130340
Max Rivera <hellomauriciorivera@gmail.com>
2020-12-07 15:36:35 -0500

    button renders in the right place. Next is the styling

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import {
   StyleSheet,
   View,
@@ -11,15 +11,15 @@ import {
   ScrollView,
   LogBox,
   FlatList,
-} from 'react-native';
-import MapboxGL from '@react-native-mapbox-gl/maps';
-import firestore from '@react-native-firebase/firestore';
-import Geolocation from 'react-native-geolocation-service';
-import { MAPBOXGL_ACCESS_TOKEN } from './secrets';
-import BottomSheet from 'reanimated-bottom-sheet';
-import { browse } from './foursquare';
-import renderAnnotation from './renderAnnotation';
-import { renderInner, renderHeader } from './drawer';
+} from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+import firestore from "@react-native-firebase/firestore";
+import Geolocation from "react-native-geolocation-service";
+import { MAPBOXGL_ACCESS_TOKEN } from "./secrets";
+import BottomSheet from "reanimated-bottom-sheet";
+import { browse } from "./foursquare";
+import renderAnnotation from "./renderAnnotation";
+import { renderInner, renderHeader } from "./drawer";
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 MapboxGL.setAccessToken(MAPBOXGL_ACCESS_TOKEN);
@@ -45,10 +45,10 @@ class App extends React.Component {
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
       ]).then((result) => {
-        console.log('result', result);
+        console.log("result", result);
         if (
-          result['android.permission.ACCESS_COARSE_LOCATION'] &&
-          result['android.permission.ACCESS_FINE_LOCATION'] === 'granted'
+          result["android.permission.ACCESS_COARSE_LOCATION"] &&
+          result["android.permission.ACCESS_FINE_LOCATION"] === "granted"
         ) {
           this.getUserLocation();
           this.setState({
@@ -57,45 +57,45 @@ class App extends React.Component {
         }
       });
     } catch (err) {
-      console.warn('err', err);
+      console.warn("err", err);
     }
   }
 
   getUserLocation() {
     Geolocation.getCurrentPosition(
       (position) => {
-        console.log('getUserLocation position:', position);
+        console.log("getUserLocation position:", position);
         this.setState({
           userCoords: [position.coords.longitude, position.coords.latitude],
         });
-        console.log('this state user', this.state.userCoords);
+        console.log("this state user", this.state.userCoords);
         this.get4SqVenues();
       },
       (error) => {
         // See error code charts below.
-        console.log('getUserLocation error:', error.code, error.message);
+        console.log("getUserLocation error:", error.code, error.message);
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   }
 
   async componentDidMount() {
-    if (Platform.OS === 'ios') {
-      Geolocation.requestAuthorization('whenInUse').then((res) => {
-        console.log('authorization result:', res);
+    if (Platform.OS === "ios") {
+      Geolocation.requestAuthorization("whenInUse").then((res) => {
+        console.log("authorization result:", res);
       });
     }
-    if (Platform.OS === 'android') {
+    if (Platform.OS === "android") {
       this.requestPermission();
     } else {
       this.getUserLocation();
     }
     await this.getFirestoreLocations();
-    console.log('this.state.locations', this.state.locations);
+    console.log("this.state.locations", this.state.locations);
   }
 
   async getFirestoreLocations() {
-    const snapshot = await firestore().collection('locations').get();
+    const snapshot = await firestore().collection("locations").get();
     return snapshot.docs.map((doc) => {
       let docObj = doc.data();
       this.setState((prevState) => {
@@ -124,28 +124,31 @@ class App extends React.Component {
   render() {
     const venuesArray = this.state.foursquare;
     return (
-      <View style={{ flex: 1, height: '100%', width: '100%' }}>
-        <Button
-          style={{ justifyContent: 'right' }}
-          title="Camera"
-          onPress={() => this.props.navigation.navigate('Camera')}
-        />
+      <View style={{ flex: 1, height: "100%", width: "100%" }}>
         {this.state.userCoords ? (
           <MapboxGL.MapView
             styleURL={MapboxGL.StyleURL.Street}
             zoomLevel={16}
             centerCoordinate={this.state.userCoords}
             showUserLocation={true}
-            style={{ flex: 1 }}
+            style={styles.map}
           >
+            <View
+              style={styles.cameraButton}
+            >
+              <Button
+                title="Camera"
+                onPress={() => this.props.navigation.navigate("Camera")}
+              />
+            </View>
             <MapboxGL.Camera
               zoomLevel={16}
               centerCoordinate={this.state.userCoords}
             ></MapboxGL.Camera>
-            {renderAnnotation('user', this.state.userCoords)}
+            {renderAnnotation("user", this.state.userCoords)}
             {this.state.locations.map((location, idx) => {
               return renderAnnotation(
-                'firestore',
+                "firestore",
                 [location.coordinates.longitude, location.coordinates.latitude],
                 idx
               );
@@ -153,7 +156,7 @@ class App extends React.Component {
             {
               venuesArray.map((venue, idx) => {
                 const { lat, lng } = venue.location;
-                return renderAnnotation('foursquare', [lng, lat], idx);
+                return renderAnnotation("foursquare", [lng, lat], idx);
               }) //renderAnnotation('foursquare', this.state.foursquare)
             }
           </MapboxGL.MapView>
@@ -171,5 +174,20 @@ class App extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+    zIndex: -1,
+  },
+  cameraButton: {
+    position: "absolute", //use absolute position to show button on top of the map
+    top: "4%", //for center align
+    alignSelf: "flex-end", //for align to right
+  }
+});
 
 export default App;
